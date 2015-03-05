3e3cd323345c5e6ee25e118a3c83a4adf37919af
Lucas Soares Marques <lmarques@inf.ufsm.br>
2015-03-05 08:42:52 -0300

    alterei

--- aula1.c
+++ aula1.c
@@ -1,1 +1,21 @@
-Aula 1
+//Aula 1 - 05/03/2014
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main (void)
+{
+	int mat;
+	char nome[50];
+	FILE *F;
+
+	printf ("Digite seu nome:");
+	scanf ("% [^\n]s", &nome);
+	
+	printf ("Digite sua matricula:");
+	scanf ("%d", &mat);
+
+	f = fopen ("saida.txt, "w");
+	fprint (f, "%d %s\n", nome, mat);
+	fclose (f);
+}
