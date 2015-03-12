680f4d7b447ab3ce9c1460350178f2988f15a54b
orphz <lmarques@inf.ufsm.br>
2015-03-12 14:13:45 -0300

    T1 1.0.1.1 - 12/03/2015

--- T1/T1.c
+++ T1/T1.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-void ler_alunos (int* mat, char** nomes, int* n)
+int pesquisar_alunos (int* mat, char** nomes, int* n, char** nomes_pesquisa)
 {
 	FILE *F = fopen("alunos.txt", "r");
-	int matricula, i, linha=0;
+	int matricula, i, linha=0, j=0;
+	int* achado;
 	char c, nome[50];
 	while (feof(F)!=0)
 	{
@@ -18,23 +19,52 @@ void ler_alunos (int* mat, char** nomes, int* n)
 		}
 		while (c!='\n')
 		{
-			nomes[i]=c;
+			nome[i]=c;
 			i++;
 		}
-		
+
 		nomes[i]='\0';
-		strcpy(nomes[linha], nome)
-		linha++
+		strcpy(nomes[linha], nome);
+		mat[linha]=matricula;
+		linha++;
 	}
 	*n=linha;
 	fclosef(F);
 }
 
+int pesquisa (char** nomes, char** nome_pesquisa, int* n)
+{
+    int mat_found;
+    for (i=0; i<=n; i++)
+    {
+        if (strstr(nomes[i], nome_pesquisa)!=NULL)
+        {
+            mat_found[i]=mat[i];
+        }
+    }
+    return mat_found;
+}
+
+void ler_notas (int* mat_found, int* n)
+{
+    FILE *F = fopen("alunos.txt", "r");
+    int i=0;
+    int* matricula, n1, n2, nf;
+    while (i<n)
+    {
+        fscanf (f,"%d" "%f" "%f", &matricula[i], &n1[i], &n2[i]);
+        nf[i]=(n1[i]+n2[i])/2;
+        i++;
+    }
+}
+
 int main (int argc, char **argv)
 {
 	char *nome;
-	char nome[50][50]
-	int mat[50], n;
+	char nomes[50][50];
+	char nomes_pesquisa[50][50];
+	int mat[50];
+	int *notas;
 
 	if(argc>0)
 	{
